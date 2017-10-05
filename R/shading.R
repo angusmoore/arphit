8f99336364523959e71d1a14b09ff7a4efd8aebe
angusmoore <angus.moore@internode.on.net>
2017-10-05 17:15:06 +1100

    Remove duplicate handling
    Should have been removed as part of duplicate handling refactor

--- R/shading.R
+++ R/shading.R
@@ -1,51 +1,13 @@
 shadingsanity <- function(to, from, panels) {
   if (!(to %in% names(panels$serieslist))) {
-    stop(paste("Series ", to, " from your shading options is not a recognised series.", sep = ""))
+    stop(paste("Series ", to, " from your shading options is not a recognised series. Perhaps it was a duplicate (which get renamed by arphit internally)?", sep = ""))
   }
   if (!(from %in% names(panels$serieslist))) {
-    stop(paste("Series ", from, " from your shading options is not a recognised series.", sep = ""))
+    stop(paste("Series ", from, " from your shading options is not a recognised series. Perhaps it was a duplicate (which get renamed by arphit internally)?", sep = ""))
   }
-  if (to %in% panels$duplicates && from %in% panels$duplicates) {
-    stop(paste("Both ", to, " and ", from, " are non-uniquely identified across panels. Cannot shade between them.", sep = ""))
-  }
-}
-
-handleshading_duplicates <- function(shading, panels) {
-  out <- list()
-  i <- 0
-  for (s in shading) {
-    # Sanity check
-    shadingsanity(s$to, s$from, panels)
-    i <- i + 1
-    if (s$to %in% panels$duplicates) {
-      # Check for which panel other series is in
-      p <- panels$serieslist[[s$from]]
-      out[[i]] <- list(to = paste(s$to, p, sep = ""), from = s$from, color = s$color)
-    } else {
-      out[[i]] <- s
-    }
-  }
-  shading <- out
-
-  # Repeat, with from
-  out <- list()
-  i <- 0
-  for (s in shading) {
-    i <- i + 1
-    if (s$from %in% panels$duplicates) {
-      # Check for which panel other series is in
-      p <- panels$serieslist[[s$to]]
-      out[[i]] <- list(to = s$to, from = paste(s$from, p, sep = ""), color = s$color)
-    } else {
-      out[[i]] <- s
-    }
-  }
-  return(out)
 }
 
 handleshading <- function(shading, panels) {
-
-  shading <- handleshading_duplicates(shading, panels)
 
   out <- list()
   for (p in names(panels$panels)) {
