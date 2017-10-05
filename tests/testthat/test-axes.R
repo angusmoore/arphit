f2f6fc1eb221b2b797a3a7bf6467176dea6a9ea9
angusmoore <angus.moore@internode.on.net>
2017-10-05 17:09:48 +1100

    Add tests for axes errors
    Ensure that errors are thrown for invalid inputs to the axes handling functions

--- tests/testthat/test-axes.R
+++ tests/testthat/test-axes.R
@@ -15,6 +15,8 @@ expect_that(ylimconform(onesided, list("1" = sublist), fakedata, "1"), equals(li
 ylim <- ylimconform(onesided, NULL, fakedata, "1")
 expect_that(handleticks(fakedata, onesided, ylim), equals(list("1" = c(0,3,6,9,12), "2" = c(0,3,6,9,12))))
 
+expect_error(ylimconform(onesided, list("1" = list("min" = 1, "max" = 2, "nsteps" = 1)), fakedata, "1"))
+
 context("Default scale")
 # Test that scale create the right thing
 expect_that(createscale(0,3,4), equals(0:3))
@@ -46,4 +48,4 @@ xlim <- xlimconform(onesided, NULL, fakedata)
 xlabels <- handlexlabels(onesided, xlim)
 test_that(xlabels, equals(1.5:10.5))
 
-message("Missing tests for warn if x diff")
+expect_warning(xlimconform(twosided, list("1" = c(2000,2010), "2" = c(2001,2009)), fakedata))
