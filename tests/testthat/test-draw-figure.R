11dfd430204291d60fba2ce688df4aaa9324a9a2
angusmoore <angus.moore@internode.on.net>
2017-10-06 15:44:24 +1100

    Improved test coverage for draw-figure

--- tests/testthat/test-draw-figure.R
+++ tests/testthat/test-draw-figure.R
@@ -1,10 +1,29 @@
-# - left-right padding
-# - countsrclines
-# - counttitlelines
-# - getfigsize
-# - handle layout
-# not sure how to test figuresetup and createfigure - probably just part of integration tests?
+context("Draw figure")
 
+height <- 10 + (1 + 2)*CSI
+width <- 20 + (3 + 4)*CSI
+expect_that(getfigsize(c(10,20), 1, 2, 3, 4), equals(list(height = height, width = width)))
+
+expect_error(handlelayout("sdf"))
+
+expect_that(countfnlines("asdf"), equals(3.5))
+expect_that(countfnlines(list("asdf","asdf")), equals(4.6))
+expect_that(countfnlines(list("asdf","as\ndf")), equals(5.7))
+expect_that(countfnlines(list("as\ndf","as\ndf")), equals(6.8))
+
+expect_that(counttitlelines("foo", NULL), equals(2.7))
+expect_that(counttitlelines(NULL, NULL), equals(0.99))
+expect_that(counttitlelines(NULL, "foo"), equals(2.14))
+expect_that(counttitlelines("Foo", "foo"), equals(3.85))
+expect_that(counttitlelines("Foo\nbar", "foo"), equals(5.75))
+expect_that(counttitlelines("Foo\nbar", "foo\nbar"), equals(7.55))
+
+expect_that(countsrclines(list(text = "ASDF", plural = FALSE)), equals(1.7))
+expect_that(countsrclines(list(text = "ASDF, Foo, Bar", plural = TRUE)), equals(1.7))
+expect_that(countsrclines(list(text = "ASDF, FoA\no, Bar", plural = TRUE)), equals(2.8))
+
+expect_that(is.even(2), is_true())
+expect_that(is.even(3), is_false())
 
 expect_error(finddevice("abc.xyz"))
 expect_that(finddevice("abc.png"), equals("png"))
