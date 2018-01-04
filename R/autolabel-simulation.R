ef8b76e19cc0895d893892b7f6eea99fa31afafc
angusmoore <angus.moore@internode.on.net>
2018-01-05 09:10:45 +1100

    Reduce the number of repulsion points in granular data to improve performance

--- R/autolabel-simulation.R
+++ R/autolabel-simulation.R
@@ -1,7 +1,7 @@
 
-distanceininches <- function(x, y, serie.x, serie.y) {
-  height <- (y - serie.y) / (graphics::par("usr")[4] - graphics::par("usr")[3]) * graphics::par("pin")[2]
-  width <- (x - serie.x) / (graphics::par("usr")[2] - graphics::par("usr")[1]) * graphics::par("pin")[1]
+distanceininches <- function(x, y, a, b) {
+  height <- (y - b) / (graphics::par("usr")[4] - graphics::par("usr")[3]) * graphics::par("pin")[2]
+  width <- (x - a) / (graphics::par("usr")[2] - graphics::par("usr")[1]) * graphics::par("pin")[1]
   return(sqrt(height^2 + width^2))
 }
 
@@ -29,25 +29,15 @@ hookesattraction <- function(a, b, x, y, stiffness) {
 }
 
 seriesforce <- function(a, b, series.x, series.y) {
-  vector <- c(0, 0)
-  for (i in 1:(length(series.x)-1)) {
-    x1 <- series.x[[i]]
-    y1 <- series.y[[i]]
-    x2 <- series.x[[i+1]]
-    y2 <- series.y[[i+1]]
-    n <- ceiling(50 / length(series.x))
-
-    xseq <- seq(from = x1, to = x2, length.out = n)
-    yseq <- seq(from = y1, to = y2, length.out = n)
-    for (i in 1:n) {
-      x <- xseq[i]
-      y <- yseq[i]
-      vector <- vector + coulombrepulsion(a, b, x, y, SERIESREPULSION)
-    }
-  }
+  # vector <- c(0, 0)
+  # for (i in 1:(length(series.x)-1)) {
+  #   x <- series.x[[i]]
+  #   y <- series.y[[i]]
+  #   vector <- vector + coulombrepulsion(a, b, x, y, SERIESREPULSION)
+  # }
+  vector <- sum(coulombrepulsion(a, b, series.x, series.y, SERIESREPULSION))
   return(vector)
 }
-
 calculate.seriesforces <- function(data, labelsmap, label, a, b) {
   vector <- c(0, 0)
   for (s in names(labelsmap)) {
@@ -92,7 +82,6 @@ calculate.repulsion <- function(l, label, data, labelsmap, labellocations, xlim,
   b <- l[2]
   vector <- calculate.seriesforces(data, labelsmap, label, a, b)
   vector <- vector + calculate.labelsforce(labellocations, label, a, b)
-  vector <- vector + calculate.boundingforce(a, b, xlim, ylim)
   return(asunits(vector))
 }
 
@@ -101,9 +90,11 @@ calculate.repulsion <- function(l, label, data, labelsmap, labellocations, xlim,
 movelabel <- function(label, l, v, anchor, data, labelsmap, labellocations, xlim, ylim, ylim_n, timestep, dampening) {
   absforce <- 0
   repulsiveforce <- calculate.repulsion(l, label, data, labelsmap, labellocations, xlim, ylim, ylim_n)
-  attractiveforce <- asunits(hookesattraction(l[1], l[2], anchor[1], anchor[2], ANCHORSTIFFNESS))
+  anchorforce <- asunits(hookesattraction(l[1], l[2], anchor[1], anchor[2], ANCHORSTIFFNESS))
+  bounds <- calculate.boundingforce(l[1], l[2], xlim, ylim)
+
   # assume mass 1, therefore force = acceleration
-  acceleration <- repulsiveforce + attractiveforce
+  acceleration <- repulsiveforce + anchorforce + bounds
 
   # Move the label
   l <- l + dampening * v * timestep + 1/2 * acceleration * (timestep ^ 2)
@@ -113,17 +104,22 @@ movelabel <- function(label, l, v, anchor, data, labelsmap, labellocations, xlim
   return(list(l = l, v = v))
 }
 
-location.fromanchor <- function(label, anchor, data, labelsmap, labellocations, xlim, ylim, ylim_n) {
-  l <- c(stats::runif(min = xlim[1], max = xlim[2], n = 1), stats::runif(min = ylim[1], max = ylim[2], n = 1))
+location.fromanchor <- function(label, anchor, reduceddata, originaldata, labelsmap, labellocations, xlim, ylim, ylim_n) {
+  l <- 0.1 * c(stats::runif(min = xlim[1], max = xlim[2], n = 1), stats::runif(min = ylim[1], max = ylim[2], n = 1)) +
+    0.9 * anchor
   v <- c(stats::rnorm(n = 1, sd = (xlim[2] - xlim[1])/10), stats::rnorm(n = 1, sd = (ylim[2] - ylim[1])/10))
 
   i <- 0
+  # DEBUG
+  # simcol <- sample(colors(), 1)
   while (abs(distanceininches(v[1], v[2], 0, 0)) > MINMOVEMENTSPEED && i < MAXSIMSTEPS) {
     i <- i + 1
-    step <- movelabel(label, l, v, anchor, data, labelsmap, labellocations, xlim, ylim, ylim_n, TIMESTEP, DAMPENING)
+    step <- movelabel(label, l, v, anchor, reduceddata, labelsmap, labellocations, xlim, ylim, ylim_n, TIMESTEP, DAMPENING)
     l <- step$l
+    # DEBUG
+    # graphics::points(l[1],l[2],col = simcol)
     v <- step$v
-    if (!checkcollisions(labelsmap[[label]], l[1], l[2], data, names(labelsmap), labellocations, labelsmap, xlim, ylim, ylim_n)) {
+    if (!checkcollisions(labelsmap[[label]], l[1], l[2], originaldata, names(labelsmap), labellocations, labelsmap, xlim, ylim, ylim_n)) {
       labellocations[[label]] <- l
       return(l)
     }
@@ -133,10 +129,7 @@ location.fromanchor <- function(label, anchor, data, labelsmap, labellocations, 
 }
 
 sampleanchorpoints <- function(data, labelsmap) {
-  s1 <- sample(1:nrow(data), length(labelsmap), replace = TRUE)
-  s2 <- sample(1:nrow(data), length(labelsmap), replace = TRUE)
-  ap <- round((s1 + s2) / 2) # This biases the sample towards central anchor points
-  ap <- as.list(ap)
+  ap <- sample(1:nrow(data), length(labelsmap), replace = TRUE)
   names(ap) <- names(labelsmap)
   return(ap)
 }
@@ -145,10 +138,22 @@ labelsimulation <- function(data, labelsmap, xlim, ylim, ylim_n) {
 
   labellocations <- list()
 
+  # Reduce the data to the number of points we want to calculate repulsion from if needed.
+  # This is purely for performance when faced with a large number of points.
+  if (nrow(data) > REPULSIONPOINTS/2) {
+    # need to collapse
+    s <- ceiling(nrow(data) / REPULSIONPOINTS)
+    reduceddata <- data[seq(1, nrow(data), s), ]
+  } else {
+    reduceddata <- data
+  }
+
+  # Find some sample anchor points
   ap <- sampleanchorpoints(data, labelsmap)
+
   for (label in names(labelsmap)) {
     anchor <- c(stats::time(data)[ap[[label]]] + 1.0/(2*stats::frequency(data)), data[ap[[label]], label])
-    labellocations[[label]] <- location.fromanchor(label, anchor, data, labelsmap, labellocations, xlim, ylim, ylim_n)
+    labellocations[[label]] <- location.fromanchor(label, anchor, reduceddata, data, labelsmap, labellocations, xlim, ylim, ylim_n)
   }
   return(labellocations)
 }
