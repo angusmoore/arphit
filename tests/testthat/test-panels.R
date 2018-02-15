82d0a6a84c005d41c8781ae9abedf291a73f2fcb
angusmoore <angus.moore@internode.on.net>
2018-02-15 16:58:16 +1100

    Add new panel handling tests

--- tests/testthat/test-panels.R
+++ tests/testthat/test-panels.R
@@ -1,1 +1,27 @@
 context("Panel handling")
+
+# Basic panel handling - not providing panel ids
+expect_that(handlepanels(c("x","y","z"), "1"), equals(list("1" = c("x", "y", "z"), "2" = NULL)))
+expect_that(handlepanels(c("x","y","z"), "2h"), equals(list("1" = c("x", "y", "z"), "2" = NULL, "3" = NULL, "4" = NULL)))
+
+# Provide panel ids
+expect_that(handlepanels(list("1" = c("x","y","z")), "1"), equals(list("1" = c("x", "y", "z"), "2" = NULL)))
+expect_that(handlepanels(list("1" = c("x","y"), "2" = "z"), "1"), equals(list("1" = c("x", "y"), "2" = "z")))
+
+# Error if provide panel id not in layout
+expect_error(handlepanels(list("3" = "a"), "1"))
+
+# Duplicate
+expect_that(handlepanels(list("1" = "x", "2" = "x"), "1"), equals(list("1" = "x", "2" = "x")))
+
+# Error if invalid layout
+expect_error(handlepanels("x", "foo"))
+
+context("Bar handling")
+
+panels <- handlepanels(list("1" = c("x","y"), "2" = c("x","z")), "1")
+expect_that(handlebars(panels, TRUE), equals(list("1" = c("x","y"), "2" = c("x","z"))))
+expect_that(handlebars(panels, "z"), equals(list("2" = "z")))
+expect_that(handlebars(panels, "x"), equals(list("1" = "x", "2" = "x")))
+expect_that(handlebars(panels, c("x","z")), equals(list("1"="x","2"=c("x","z"))))
+expect_that(handlebars(panels, list("2" = c("x","z"))), equals(list("2"=c("x","z"))))
