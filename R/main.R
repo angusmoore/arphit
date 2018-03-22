2cc65eb9cb3d34a9b2a197fc2f6ba8e4be574478
angusmoore <18376371+angusmoore@users.noreply.github.com>
2018-03-22 13:07:49 +1100

    Split panel titles

--- R/main.R
+++ R/main.R
@@ -80,19 +80,19 @@ arphit <- function(data, series = NULL, x = NULL, layout = "1", bars = NULL, fil
   bgshading <- sanitycheckbgshading(bgshading)
   lines <- sanitychecklines(lines)
 
-  # Format footnotes and sources
+  # Format titles, footnotes and sources
   footnotes <- formatfn(footnotes)
   sources <- formatsrcs(sources)
   if (!is.null(title)) {
     title <- splitoverlines(title, LINELENGTHTITLE)
   }
   if (!is.null(subtitle)) {
-    subtitle <- splitoverlines(subtitle, LINELENGTHSUBTITLE)
+    subtitle <- splitoverlines(subtitle, LINELENGTHTITLE)
   }
 
   # Conform panel titles
-  paneltitles <- conformpaneltitles(panels, paneltitles)
-  panelsubtitles <- conformpaneltitles(panels, panelsubtitles)
+  paneltitles <- conformpaneltitles(panels, paneltitles, layout, LINELENGTHPANELTITLE)
+  panelsubtitles <- conformpaneltitles(panels, panelsubtitles, layout, LINELENGTHPANELSUBTITLE)
 
   # Now need to start the canvas
   device <- finddevice(filename)
