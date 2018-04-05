f1fb752cd26855fc93e566c19cbaa52ad9e0636b
angusmoore <18376371+angusmoore@users.noreply.github.com>
2018-04-05 17:03:34 +0900

    Add more layouts (#63)
    Add 3v, 3h, 3b2, 4h, 4b2 layouts

--- R/draw-figure.R
+++ R/draw-figure.R
@@ -119,8 +119,23 @@ handlelayout <- function(layout) {
   } else if (layout == "2b2") {
     graphics::par(mfrow=c(2,2))
     graphics::par(cex = 1)
+  } else if (layout == "3v") {
+    graphics::par(mfrow=c(1,3))
+    graphics::par(cex=1)
+  } else if (layout == "3h") {
+    graphics::par(mfrow=c(3,1))
+    graphics::par(cex=1)
+  } else if (layout == "3b2") {
+    graphics::par(mfrow=c(3,2))
+    graphics::par(cex=1)
+  } else if (layout == "4h") {
+    graphics::par(mfrow=c(4,1))
+    graphics::par(cex=1)
+  } else if (layout == "4b2") {
+    graphics::par(mfrow=c(4,2))
+    graphics::par(cex=1)
   } else {
-    stop(paste("Unknown layout option ", layout, ". Options are 1, 2h, 2v, 2b2.", sep = ""))
+    stop(paste("Unknown layout option ", layout, ". Options are 1, 2h, 2v, 2b2, 3v, 3h, 3b2, 4h, 4b2.", sep = ""))
   }
 }
 
