754657ac81c86ee98282b6befa62b757c1fa61d3
Angus <angus.moore@internode.on.net>
2018-04-16 07:42:24 +1000

    Extra test to improve coverage

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -156,3 +156,4 @@ expect_error(agg_draw(p, filename = "my-graph.png"), NA)
 # #71 incorrect handling of NAs
 bar <- data.frame(x = c(2000:2003), y = c(NA, rnorm(3)), z = c(NA, y = rnorm(3)))
 expect_error(arphit(bar, x="x"), NA)
+expect_error(arphit(bar, x="x", joined = FALSE), NA)
