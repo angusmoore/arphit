ee4645d69408a18e648abdf154ec447401f6cb15
Angus <angus.moore@internode.on.net>
2018-04-19 07:28:57 +1000

    Order facets before assigning to panels

--- tests/testthat/test-gg.R
+++ tests/testthat/test-gg.R
@@ -363,5 +363,10 @@ bar <- arphitgg() + agg_line(agg_aes(y=x1),data=foo) + agg_col(agg_aes(y=x2),dat
 expect_equal(colnames(bar$data[["1"]]), c("agg_time", "x1", "x2"))
 expect_error(print(bar), NA)
 
+# Order facets
+foo <- data.frame(x=c(1,2,1,2),y=rnorm(4),facet=c("b","b","a","a"), stringsAsFactors = FALSE)
+bar <- arphitgg(foo,agg_aes(x=x,y=y,facet=facet))+agg_col()
+expect_equal(bar$paneltitles, list("1" = "a", "3" = "b"))
+
 # Shutdown any devices
 graphics.off()
