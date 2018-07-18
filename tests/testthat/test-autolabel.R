4d748686050128d81ad96279cee8eb5909bada31
angusmoore <18376371+angusmoore@users.noreply.github.com>
2018-07-18 15:53:31 +1000

    Handle NAs in autolabeller (#139)

--- tests/testthat/test-autolabel.R
+++ tests/testthat/test-autolabel.R
@@ -26,3 +26,40 @@ expect_error(
   print(p),
   NA
 )
+
+context("Autolabeller - miscellaneous tests")
+foo <- data.frame(x = 1:50, y = rnorm(50), y2 = rnorm(50))
+
+p <- arphitgg(foo) + agg_line(agg_aes(x=x,y=y)) + agg_line(agg_aes(x=x,y=y2)) + agg_autolabel()
+expect_error(
+  print(p),
+  NA
+)
+
+# Non-numeric anchor point (#122)
+data <- tibble::tibble(x = sort(rep(letters[1:5],2)), g = rep(c("f","m"),5), y = rnorm(10))
+p <-  arphitgg(data, agg_aes(x=x,y=y,group=g)) +
+  agg_col() +
+  agg_autolabel()
+expect_error(
+  print(p),
+  NA
+)
+
+# Factors in data for auto labeller (#123)
+p <- data.frame(x = sort(rep(letters[1:5],2)), g = rep(c("f","m"),5), y = rnorm(10)) %>%
+  arphitgg(agg_aes(x=x,y=y,group=g)) +
+  agg_col() +
+  agg_autolabel()
+expect_error(
+  print(p),
+  NA
+)
+
+# NAs in data for autolabeller (#126)
+foo <- tibble::tibble(year = 2000:2020, y = rnorm(21),y2=rnorm(21))
+foo$y[1:10] <- NA
+expect_error(
+  agg_qplot(foo, x="year", enable_autolabeller = TRUE),
+  NA
+)
