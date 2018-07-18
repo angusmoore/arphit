2f1bf663fd233d6b4600c0fac79feb360041f2ac
Angus <18376371+angusmoore@users.noreply.github.com>
2018-07-18 21:44:57 +1000

    Bad commit on test

--- tests/testthat/test-autolabel.R
+++ tests/testthat/test-autolabel.R
@@ -59,7 +59,7 @@ expect_error(
 # NAs in data for autolabeller (#126)
 foo <- tibble::tibble(year = 2000:2020, y = rnorm(21),y2=rnorm(21))
 foo$y[1:10] <- NA
-p <- arphitgg(foo, agg_aes(x=x)) +
+p <- arphitgg(foo, agg_aes(x=year)) +
   agg_line(agg_aes(y=y)) +
   agg_line(agg_aes(y=y2)) +
   agg_autolabel()
