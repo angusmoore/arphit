e854ef32f44a9e4508ecf2b3352a2010c858710c
angusmoore <18376371+angusmoore@users.noreply.github.com>
2018-07-18 10:06:46 +1000

    Handle NAs

--- R/autolabel-distances.R
+++ R/autolabel-distances.R
@@ -1,27 +1,31 @@
 pointlinedistance <- function(x, y, x1, y1, x2, y2) {
-  A <- x - x1
-  B <- y - y1
-  C <- x2 - x1
-  D <- y2 - y1
-
-  dot <- A * C + B * D
-  len_sq <- C * C + D * D
-  param <- -1
-  if (len_sq != 0) {
-    param = dot / len_sq
-  }
-
-  if (param < 0) {
-    xx <- x1
-    yy <- y1
-  } else if (param > 1) {
-    xx <- x2
-    yy <- y2
+  if(is.na(x) || is.na(y) || is.na(x1) || is.na(y1) || is.na(x2) || is.na(y2)) {
+    return(list(dist = Inf, x = NA, y = NA))
   } else {
-    xx <- x1 + param * C
-    yy <- y1 + param * D
+    A <- x - x1
+    B <- y - y1
+    C <- x2 - x1
+    D <- y2 - y1
+
+    dot <- A * C + B * D
+    len_sq <- C * C + D * D
+    param <- -1
+    if (len_sq != 0) {
+      param <- dot / len_sq
+    }
+
+    if (param < 0) {
+      xx <- x1
+      yy <- y1
+    } else if (param > 1) {
+      xx <- x2
+      yy <- y2
+    } else {
+      xx <- x1 + param * C
+      yy <- y1 + param * D
+    }
+    return (list(dist = distanceininches(x, y, xx, yy), x = xx, y = yy))
   }
-  return (list(dist = distanceininches(x, y, xx, yy), x = xx, y = yy))
 }
 
 lineofsight.point2point <- function(x, y, a, b, block.x, block.y) {
