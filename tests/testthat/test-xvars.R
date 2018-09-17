f42b88ae6ed16da3311e459ad75d0e1447c7979c
Angus <18376371+angusmoore@users.noreply.github.com>
2018-09-17 20:06:25 +1000

    Refactor data and x handling. Order data by dates in time series plot.

--- tests/testthat/test-xvars.R
+++ tests/testthat/test-xvars.R
@@ -59,7 +59,6 @@ data <- data.frame(year = c(1991, 2001, 2006, 2011, 2016), y = rnorm(5))
 data$year <- as.Date(paste0(data$year, "-01-01"))
 expect_that(get_x_values(handledata(NULL, list("1" = data), list("1" = "year"))$data, list("1" = "year"))[["1"]],
             equals(c(1991.5, 2001.5, 2006.5, 2011.5, 2016.5)))
-
 # Test if don't specify x variable for qplot
 expect_error(
   agg_qplot(data.frame(y=1:10)),
