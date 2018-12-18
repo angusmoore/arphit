cda7aa0787937114bf4b69683dab09573da1022f
Angus Moore <18376371+angusmoore@users.noreply.github.com>
2018-12-18 11:02:33 +1100

    Testing for singleton series was ignoring RHS axes

--- R/autolabel-utils.R
+++ R/autolabel-utils.R
@@ -15,7 +15,15 @@ createlabels <- function(panels, bars, attributes, layout, labels) {
   # Remove any single-panel series if that panel has only one series (doesn't need a label!)
   for (series in names(series_to_panels)) {
     if (length(series_to_panels[[series]]) == 1 && length(panels[[series_to_panels[[series]]]]) == 1) {
-      series_to_panels[series] <- NULL
+      # check there aren't series on alternative axes
+      other_p <- other_axes(series_to_panels[[series]], layout)
+      if (!is.null(other_p)) {
+        if(!length(panels[[other_p]]) > 0) {
+          series_to_panels[series] <- NULL
+        }
+      } else {
+        series_to_panels[series] <- NULL
+      }
     }
   }
 
