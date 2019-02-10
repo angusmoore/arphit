42c4b71f9ed64b17c9b016cd5f49dee0cc432181
Angus <18376371+angusmoore@users.noreply.github.com>
2019-02-10 15:01:16 +1100

    Add tests for some of the qplot features

--- tests/testthat/test-qplot.R
+++ tests/testthat/test-qplot.R
@@ -7,6 +7,8 @@ test_that("Simple smoke tests of qplot", {
   data <- ts(data.frame(y=rnorm(10),y2=rnorm(10),y3=rnorm(10)), frequency = 4, start = 2000)
   expect_error(agg_qplot(data, series = c("y","y2")), NA)
   expect_error(agg_qplot(data, series = c("y","y2"), bars = TRUE), NA)
+  expect_error(agg_qplot(data, xlim = c(2000,2010),ylim=list(min=-10,max=10,nsteps=5),legend=TRUE,pch=19), NA)
+  expect_error(agg_qplot(data, pch=NULL), NA)
 })
 
 test_that("Errors for wrong x vars", {
