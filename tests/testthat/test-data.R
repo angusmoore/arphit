bbf628284e9063ec06181ce30d72211045b54d57
angusmoore <18376371+angusmoore@users.noreply.github.com>
2019-03-11 16:57:28 +1100

    Widening of x values in bar graphs needs to be fuzzy

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -94,3 +94,13 @@ test_that("zoo and xts", {
   expect_error(agg_qplot(bar),NA)
   expect_true(check_graph(p, "data-xts")) # Should be identical to the xts version
 })
+
+
+## Placement of bars ================
+
+test_that("Bar graph placement", {
+  set.seed(42)
+  foo <- data.frame(date=seq.Date(from = as.Date("2000-01-01"),by="month",length.out=12*4),y=rnorm(12*4))
+  p <- arphitgg(foo, agg_aes(x=date,y=y))+agg_line()+agg_col()
+  expect_true(check_graph(p, "data-bar-placement"))
+})
