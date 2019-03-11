17e737b04f7ee94a0c85236c8410482956f186c3
Angus <18376371+angusmoore@users.noreply.github.com>
2019-03-12 06:39:13 +1100

    Typo fix and linux reference

--- tests/testthat/test-axes.R
+++ tests/testthat/test-axes.R
@@ -223,7 +223,7 @@ test_that("Basic x axis", {
   set.seed(42)
   foo <- data.frame(x=rnorm(10),y=rnorm(10))
   p <- arphitgg(foo, agg_aes(x=x,y=y))+agg_point()+agg_xlim(-0.9,1)
-  expectr_true(check_graph(p, "axes-x-scatter-xlimits"))
+  expect_true(check_graph(p, "axes-x-scatter-xlimits"))
 })
 
 test_that("Warning for different limits",{
