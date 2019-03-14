577e18f06eb0b273df365b635b682ae684756376
Max Sutton <max.sutton@outlook.com.au>
2019-03-15 09:23:43 +1100

    Add namespace to image_read

--- R/animate.R
+++ R/animate.R
@@ -15,7 +15,7 @@ agg_slides <- function(gg_list, filename = NULL, delay = 1, loop = 0) {
   img_list <- lapply(seq_along(gg_list), function(idx, gg_list) {
     file_name <- file.path(tempdir(), paste0("animate-", idx, ".png"))
     agg_draw(gg_list[[idx]], file_name)
-    list(path = file_name, img = image_read(file_name))
+    list(path = file_name, img = magick::image_read(file_name))
   }, gg_list)
 
   on.exit(unlink(sapply(img_list, "[[", "path")))
