f7fd84e8980afd2a6bc53052683f3d935372413d
angusmoore <18376371+angusmoore@users.noreply.github.com>
2019-03-26 15:52:56 +1100

    Split tests into multiple files

---
+++ tests/k-o.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(arphit)
+
+test_check("arphit", filter = "^[k-o]")
--- tests/testthat.R
+++
@@ -1,5 +0,0 @@
-library(testthat)
-library(arphit)
-
-test_check("arphit", "test-[a-g]")
-test_check("arphit", "test-[h-z]")
